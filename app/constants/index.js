b6dc04bc005116318bf52dbbf4c953e3ac8129c5
葛彦斌 <pamler@126.com>
2017-10-20 17:02:46 +0800

    fix bug

---
+++ app/constants/index.js
@@ -0,0 +1,5 @@
+export const MARIO_PATH = `${process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE}/.Mario`;
+export const MARIO_CONFIG_PATH = `${MARIO_PATH}/Config`;
+export const MARIO_CONFIG_FILENAME = 'config.json';
+export const MARIO_RUNSTATE_FILENAME = 'run-state.json';
+
